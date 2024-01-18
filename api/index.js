d19d33ad5ae00c7ac5e518171c76f1d9c3e72683
Bisapogu Vamshi <bvamshi4928@gmail.com>
2024-01-18 14:57:41 -0700

    create signup route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
+//Routes
+import userRouter from "./routes/userRoute.js";
+import authRouter from "./routes/authRoute.js";
+
 dotenv.config();
 
 mongoose
@@ -14,7 +18,11 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server running ");
 });
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
