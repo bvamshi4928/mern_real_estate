127dfcbcb500cc1770fb9dcbce5f4bfc6d9e4a2a
Bisapogu Vamshi <bvamshi4928@gmail.com>
2024-01-21 11:59:59 -0700

    signin api route created

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +11,30 @@ export const signup = async (req, res, next) => {
     // console.log(newUser);
     await newUser.save();
     res.status(201).json("new user created successfully");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) return next(errorHandler(401, "wrong crenditals!"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (err) {
     next(err);
   }
