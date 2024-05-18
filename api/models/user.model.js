2893a35d7f9f9006d8f8c449a658197ea409d9d8
Bisapogu Vamshi <bvamshi4928@gmail.com>
2024-05-18 10:09:33 -0700

    Revert "Create and add Google OAuth"
    This reverts commit f3342fa33ea8a6069704c735d9a3c3fcc2f9dc69.

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,10 +16,6 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    avatar: {
-      type: String,
-      default:"https://t3.ftcdn.net/jpg/03/46/83/96/240_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
-    },
   },
   { timestamps: true }
 );
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
-import OAuth from "../components/OAuth.jsx";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({});
@@ -71,7 +70,6 @@ const SignUp = () => {
         >
           {loading ? "Loading..." : "Sign-up"}
         </button>
-        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
