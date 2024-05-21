66349bc3019a782794e612d0c1824e3cfe83bc62
Bisapogu Vamshi <bvamshi4928@gmail.com>
2024-05-21 09:04:46 -0700

    create user update API

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 //Routes
 
@@ -19,6 +20,7 @@ mongoose
   });
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log(`server is running on port 3000`);
