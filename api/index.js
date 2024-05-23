17739924b3fd5a9f4aa1f2720f913ea297fe293b
Bisapogu Vamshi <bvamshi4928@gmail.com>
2024-05-23 10:16:06 -0700

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 
 dotenv.config();
 
@@ -28,6 +29,9 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+//listing
+app.use("/api/listing", listingRouter);
 
 //middleware
 
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,20 +1,22 @@
 import express from "express";
-import { signup,signin,google,signout } from "../controllers/auth.controller.js";
-
+import {
+  signup,
+  signin,
+  google,
+  signout,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
-
 
 //Sign UP
 router.post("/signup", signup);
 
 //Sign IN
-router.post("/signin",signin);
-
+router.post("/signin", signin);
 
 //Google Sign In
-router.post("/google",google)
+router.post("/google", google);
 
 //Sign out
-router.get('/signout',signout)
+router.get("/signout", signout);
 export default router;
