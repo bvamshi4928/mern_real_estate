89deeb553ca41b20e60fe12ec60cba1e7b709ae6
Bisapogu Vamshi <bvamshi4928@gmail.com>
2024-05-25 11:25:20 -0700

    create getuser listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.send("hello world is also hello world");
@@ -36,6 +37,8 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
+//Delete a User
+
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, "You can only delete your account"));
@@ -46,5 +49,20 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("User has been deleted");
   } catch (error) {
     next(error);
+  }
+};
+
+//get listing
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can view your own listings!"));
   }
 };
