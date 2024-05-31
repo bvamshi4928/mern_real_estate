18c11f386c9b60ed7a867a2e372323efabfbe3df
Bisapogu Vamshi <bvamshi4928@gmail.com>
2024-05-31 14:34:43 -0700

    add pre installation

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 //Routes
 
@@ -19,6 +20,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -29,9 +32,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-
-//listing
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path, join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 //middleware
 
